cb8adc67346f6cf00fe8b8aa92a0d3081f3fefd1
Joshua Woolf <joshuawoolf87@gmail.com>
2024-03-12 19:16:20 +0200

    chore: Update PowerShell Profile

--- src/Configurations/PowerShell/Microsoft.PowerShell_profile.ps1
+++ src/Configurations/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,99 +1,39 @@
-Function Write-CustomLog {
-  [CmdletBinding()]
-  param (
-    [Parameter(Mandatory = $true)] [string] $Message
-  )
-
-  Write-Host $Message -BackgroundColor "Cyan" -ForegroundColor "Black"
+function hosts {
+	code "$(Join-Path $env:SystemRoot '\system32\drivers\etc\hosts')"
 }
 
-Function Cleanup {
-  Write-CustomLog "Emptying recycle bin..."
-  (New-Object -ComObject Shell.Application).Namespace(0xA).Items() | ForEach-Object { Remove-Item $_.Path -Recurse -Confirm:$false }
-
-  Write-CustomLog "Running disk cleanup..."
-  Sudo "$(Join-Path $env:SystemRoot 'system32\cleanmgr.exe')" "/sagerun:6174"
+function myip {
+	(Invoke-WebRequest "http://ifconfig.me/ip").Content
 }
 
-Function Download {
-  [CmdletBinding()]
-  Param (
-    [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)] [string] $Uri
-  )
-  $fileName = [System.IO.Path]::GetFileName($Uri)
-  Invoke-WebRequest -Uri $Uri -OutFile $fileName
+function ll {
+	Get-ChildItem -Force $args
 }
 
-Function Extract {
-  [CmdletBinding()]
-  Param (
-    [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)] [string] $Archive,
-    [Parameter(Position = 1, Mandatory = $true)] [string] $TargetDirectory
-  )
-
-  If (-not (Test-Path -Path $TargetDirectory -PathType Container)) {
-    New-Item -Path $TargetDirectory -ItemType Directory -Force
-  }
-
-  If ($Archive -match '\.zip$') {
-    Expand-Archive -Path $Archive -DestinationPath $TargetDirectory -Force
-  }
-  ElseIf ($Archive -match '\.tar\.gz$') {
-    tar -xzf $Archive -C $TargetDirectory
-  }
+function profile {
+	code $PROFILE
 }
 
-Function Get-PublicIpAddress {
-  (Invoke-WebRequest "http://ifconfig.me/ip").Content
+function update {
+	winget upgrade --all --accept-package-agreements --accept-source-agreements
+	Update-Module -AcceptLicense
 }
 
-Function Hosts {
-  code "$(Join-Path $env:SystemRoot '\system32\drivers\etc\hosts')"
+function which {
+	param($bin)
+	(Get-Command $bin -ErrorAction SilentlyContinue).Definition
 }
 
-Function Profile {
-  code $PROFILE
+function Invoke-Starship-PreCommand {
+	$current_location = $executionContext.SessionState.Path.CurrentLocation
+	if ($current_location.Provider.Name -eq "FileSystem") {
+		$ansi_escape = [char]27
+		$provider_path = $current_location.ProviderPath -replace "\\", "/"
+		$prompt = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}$ansi_escape\"
+	}
+	$host.ui.Write($prompt)
 }
 
-Function Sudo {
-  If ($args.Length -eq 1) {
-    Start-Process $args[0] -Verb "RunAs"
-  }
-  If ($args.Length -gt 1) {
-    Start-Process $args[0] -ArgumentList $args[1..$args.Length] -Verb "RunAs"
-  }
-}
+Invoke-Expression (&starship init powershell)
 
-Function Update {
-  Write-CustomLog "Updating Microsoft Store applications..."
-  Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName UpdateScanMethod
-
-  Write-CustomLog "Updating WinGet applications..."
-  winget upgrade --all --accept-package-agreements --accept-source-agreements
-
-  Write-CustomLog "Updating PowerShell modules..."
-  Update-Module
-
-  Import-Module PSWindowsUpdate
-
-  Write-CustomLog "Installing Windows updates..."
-  Install-WindowsUpdate -AcceptAll -IgnoreReboot -IgnoreUserInput
-}
-
-Function Version {
-  $osInfo = (Get-WmiObject Win32_OperatingSystem)
-  Write-Host "$($osInfo.Caption) ($($osInfo.Version))"
-}
-
-Function Which {
-  [CmdletBinding()]
-  Param (
-    [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)] [string] $Name
-  )
-
-  (Get-Command $Name -ErrorAction SilentlyContinue).Definition
-}
-
-oh-my-posh init pwsh --config "https://raw.githubusercontent.com/joshua-woolf/omp-themes/main/grape.omp.json" | Invoke-Expression
-
-Clear-Host
+$prompt = ""
