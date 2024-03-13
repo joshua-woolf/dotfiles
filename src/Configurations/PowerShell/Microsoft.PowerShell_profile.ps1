e4c55114bb6129d356432683ae3e0d8d7993dbf5
Joshua Woolf <joshuawoolf87@gmail.com>
2024-03-13 20:36:00 +0200

    Update Microsoft.PowerShell_profile.ps1

--- src/Configurations/PowerShell/Microsoft.PowerShell_profile.ps1
+++ src/Configurations/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,37 +1,45 @@
 function hosts {
-	code "$(Join-Path $env:SystemRoot '\system32\drivers\etc\hosts')"
+  code (Join-Path $env:SystemRoot "\system32\drivers\etc\hosts")
 }
 
 function myip {
 	(Invoke-WebRequest "http://ifconfig.me/ip").Content
 }
 
+function kubeconfig {
+  code (Join-Path $env:USERPROFILE "\.kube\config")
+}
+
 function ll {
-	Get-ChildItem -Force $args
+  Get-ChildItem -Force $args
 }
 
 function profile {
-	code $PROFILE
+  code $PROFILE
+}
+
+function q {
+  exit
 }
 
 function update {
-	winget upgrade --all --accept-package-agreements --accept-source-agreements
-	Update-Module -AcceptLicense
+  winget upgrade --all --accept-package-agreements --accept-source-agreements
+  Update-Module -AcceptLicense
 }
 
 function which {
-	param($bin)
+  param($bin)
 	(Get-Command $bin -ErrorAction SilentlyContinue).Definition
 }
 
 function Invoke-Starship-PreCommand {
-	$current_location = $executionContext.SessionState.Path.CurrentLocation
-	if ($current_location.Provider.Name -eq "FileSystem") {
-		$ansi_escape = [char]27
-		$provider_path = $current_location.ProviderPath -replace "\\", "/"
-		$prompt = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}$ansi_escape\"
-	}
-	$host.ui.Write($prompt)
+  $current_location = $executionContext.SessionState.Path.CurrentLocation
+  if ($current_location.Provider.Name -eq "FileSystem") {
+    $ansi_escape = [char]27
+    $provider_path = $current_location.ProviderPath -replace "\\", "/"
+    $prompt = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}$ansi_escape\"
+  }
+  $host.ui.Write($prompt)
 }
 
 Invoke-Expression (&starship init powershell)
