0f27a54e84074fcce0616a17c48d4fb884d4c030
Joshua Woolf <joshuawoolf87@gmail.com>
2024-11-10 20:20:03 +0200

    feat: Add macOS Config

---
+++ src/config/macos/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,32 @@
+function hosts {
+  code "/etc/hosts"
+}
+
+function kubeconfig {
+  code (Join-Path $HOME "\.kube\config")
+}
+
+function profile {
+  code $PROFILE
+}
+
+function update {
+  /opt/homebrew/bin/brew update && /opt/homebrew/bin/brew upgrade
+  Update-Module -AcceptLicense
+}
+
+function Invoke-Starship-PreCommand {
+  $current_location = $executionContext.SessionState.Path.CurrentLocation
+  if ($current_location.Provider.Name -eq "FileSystem") {
+    $ansi_escape = [char]27
+    $provider_path = $current_location.ProviderPath -replace "\\", "/"
+    $prompt = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}$ansi_escape\"
+  }
+  $host.ui.Write($prompt)
+}
+
+Invoke-Expression (&/opt/homebrew/bin/starship init powershell)
+
+$prompt = ""
+
+Clear-Host
---
+++ src/config/macos/.zshrc
@@ -0,0 +1,16 @@
+function hosts {
+  code /etc/hosts
+}
+
+function kubeconfig {
+  code "$HOME/.kube/config"
+}
+
+function update {
+  brew update && brew upgrade
+}
+
+source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+eval "$(starship init zsh)"
